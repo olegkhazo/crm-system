251c01e526b0a0683e997657f9d7e45cc95b6c25
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-03-15 15:31:06 +0200

    added lato fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxt/image", 
+    "@nuxtjs/tailwindcss", 
+    "shadcn-nuxt", 
+    [
+      "@nuxtjs/google-fonts", 
+        {
+        families: {
+          Lato: {
+            wght: [100, 300, 400, 700],
+            ital: [100]
+          },
+        },
+      },
+    ],
+  ],
 
   shadcn: {
     prefix: 'Ui',
